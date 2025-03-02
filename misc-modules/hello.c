2e9b3954d8aea4724c1c819b7582c3fb7c25ee3b
ldmacdonald <lawrence.macdonald@colorado.edu>
2025-03-01 18:26:15 -0800

    Adding username in printk()

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world %s\n", "ldmacdonald");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world %s\n", "ldmacdonald");
 }
 
 module_init(hello_init);
