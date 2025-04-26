7dfaafbc666df4dc2ee4f0bc588a8e4f448a275e
Dale MacDonald <lawrence.macdonald@colorado.edu>
2025-04-25 21:34:26 -0700

    Updating assignment 3 commit hashes.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '66e92784b5c7566b636c34581ac0b58149973e3a'
+AESD_ASSIGNMENTS_VERSION = 'c8f33d7d22e174bdd2725dd9ee460de3c26242eb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,4 +1,4 @@
-AESDCHAR_VERSION = '66e92784b5c7566b636c34581ac0b58149973e3a'
+AESDCHAR_VERSION = 'c8f33d7d22e174bdd2725dd9ee460de3c26242eb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
